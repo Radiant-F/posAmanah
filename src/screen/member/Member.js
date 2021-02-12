6f3ca0eba010fc8840fddb42bf45f29c811e68a7
RadiantF <radiantfadilah0@gmail.com>
2021-02-12 13:25:42 +0700

    first commit

---
+++ src/screen/member/Member.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class Member extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Member </Text>
+      </View>
+    );
+  }
+}
