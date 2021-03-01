dc005122a82ded832258882fabe68a556ed86c35
RadiantF <radiantfadilah0@gmail.com>
2021-03-01 21:26:01 +0700

    try datepicker

--- src/Demo.js
+++ src/Demo.js
@@ -1,66 +1,39 @@
 import React, {Component} from 'react';
-import {Image, Text, TouchableOpacity, View} from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import {Text, View} from 'react-native';
+import DatePicker from 'react-native-datepicker';
 
 export default class Demo extends Component {
   constructor() {
     super();
     this.state = {
-      avatar: '',
-      photo: '',
+      date: '2016-05-15',
     };
   }
-
-  createFormData = (photo, body) => {
-    const data = new FormData();
-    data.append('avatar', {
-      name: photo.fileName,
-      type: photo.type,
-      uri:
-        Platform.OS === 'android'
-          ? photo.uri
-          : photo.uri.replace('file://', ''),
-    });
-    Object.keys(body).forEach((key) => {
-      data.append(key, body[key]);
-    });
-    return data;
-  };
-
-  handleEditPhoto = () => {
-    const options = {
-      noData: true,
-    };
-    ImagePicker.launchImageLibrary(options, (response) => {
-      if (response.uri) {
-        this.setState({photo: response});
-        console.log(JSON.stringify(response.fileName));
-      }
-    });
-  };
-
   render() {
+    console.log(this.state.date);
     return (
-      <View>
-        <Text> textInComponent </Text>
-        <TouchableOpacity onPress={() => this.handleEditPhoto()}>
-          {this.state.photo == '' ? (
-            <View>
-              <Image
-                source={require('./assets/plainAvatar.png')}
-                style={{width: 100, height: 100}}
-              />
-            </View>
-          ) : (
-            <View>
-              <Image
-                source={{uri: this.state.photo.uri}}
-                style={{width: 100, height: 100}}
-              />
-            </View>
-          )}
-        </TouchableOpacity>
-      </View>
+      <DatePicker
+        date={this.state.date}
+        mode="date"
+        placeholder="pilih tanggal"
+        format="YYYY-MM-DD"
+        minDate="2016-05-01"
+        maxDate="2021-02-25"
+        confirmBtnText="Konfirmasi"
+        cancelBtnText="Batal"
+        customStyles={{
+          dateIcon: {
+            position: 'absolute',
+            left: 0,
+            top: 4,
+            marginLeft: 0,
+          },
+          dateInput: {
+            marginLeft: 36,
+          },
+        }}
+        onDateChange={(date) => this.setState({date: date})}
+      />
     );
   }
 }
