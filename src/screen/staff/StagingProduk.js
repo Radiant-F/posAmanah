554eb9f3d6c8c1a11ac6263686664484dd5167a7
RadiantF <radiantfadilah0@gmail.com>
2021-03-08 14:48:23 +0700

    fix styling

--- src/screen/staff/StagingProduk.js
+++ src/screen/staff/StagingProduk.js
@@ -12,6 +12,7 @@ import {
   Button,
   ToastAndroid,
   Modal,
+  Alert,
 } from 'react-native';
 import _ from 'lodash';
 
@@ -27,7 +28,7 @@ export default class StagingProduk extends Component {
       refresh: false,
       disabled: true,
       modal: false,
-      loading: false,
+      loading: true,
       loadingConfirm: false,
     };
   }
@@ -49,7 +50,6 @@ export default class StagingProduk extends Component {
 
   getPembelian() {
     this.setState({daftar_pembelian: [], loading: true});
-    ToastAndroid.show('Memuat produk...', ToastAndroid.LONG);
     console.log('mengambil daftar pembelian..');
     fetch(`https://amanah-mart.herokuapp.com/api/pembelian`, {
       method: 'GET',
@@ -60,11 +60,19 @@ export default class StagingProduk extends Component {
     })
       .then((response) => response.json())
       .then((responseJSON) => {
-        this.setState({
-          daftar_pembelian: responseJSON.data,
-          refresh: false,
-          loading: false,
-        });
+        if (responseJSON.status == 'Success') {
+          this.setState({
+            daftar_pembelian: responseJSON.data,
+            refresh: false,
+            loading: false,
+          });
+        } else {
+          this.setState({
+            loading: false,
+            daftar_pembelian: null,
+            refresh: false,
+          });
+        }
         console.log('daftar pembelian termuat.');
       })
       .catch((err) => this.fatal(err));
@@ -100,21 +108,14 @@ export default class StagingProduk extends Component {
           ToastAndroid.show('Periksa koneksi Anda.', ToastAndroid.SHORT);
         }
       })
-      .catch((error) => console.log(error));
+      .catch((err) => this.fatal(err));
   }
 
   deleteProduk(id) {
     console.log('menghapus produk..');
-    const {jumlah_product, harga, harga_jual} = this.state;
-    var dataToSend = {
-      jumlah_product: jumlah_product,
-      harga: harga,
-      harga_jual: harga_jual,
-    };
     this.setState({loading: true});
     fetch(`https://amanah-mart.herokuapp.com/api/pembelian/destroy/${id}`, {
       method: 'POST',
-      body: JSON.stringify(dataToSend),
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${this.state.token}`,
@@ -132,12 +133,12 @@ export default class StagingProduk extends Component {
           ToastAndroid.show('Periksa koneksi Anda.', ToastAndroid.SHORT);
         }
       })
-      .catch((error) => console.log(error));
+      .catch((err) => this.fatal(err));
   }
 
   confirmProduk() {
-    this.setState({loadingConfirm: true});
-    ToastAndroid.show('Mengkonfirmasi pembelian...', ToastAndroid.SHORT);
+    this.setState({loading: true});
+    ToastAndroid.show('mengkonfirmasi pembelian...', ToastAndroid.SHORT);
     console.log('mengambil daftar pembelian..');
     fetch(`https://amanah-mart.herokuapp.com/api/pembelian/confirm`, {
       method: 'POST',
@@ -148,19 +149,34 @@ export default class StagingProduk extends Component {
     })
       .then((response) => response.json())
       .then((responseJSON) => {
+        console.log('pembelian dikonfirmasi');
         ToastAndroid.show('Konfirmasi sukses', ToastAndroid.SHORT);
         this.setState({
-          daftar_pembelian: responseJSON.data,
+          daftar_pembelian: null,
           refresh: false,
-          loadingConfirm: false,
         });
-        console.log('daftar pembelian termuat.');
+        this.getPembelian();
       })
-      .catch((err) => console.log(err));
+      .catch((err) => this.fatal(err));
   }
 
   fatal(err) {
-    this.setState({refresh: false});
+    console.log(err);
+    this.setState({refresh: false, loading: false, daftar_pembelian: null});
+    Alert.alert(
+      'Koneksi Tidak Stabil',
+      'Coba lagi beberapa saat.',
+      [
+        {
+          text: 'Ok',
+        },
+        {
+          text: 'Ulangi',
+          onPress: () => this.getToken(),
+        },
+      ],
+      {cancelable: false},
+    );
   }
 
   render() {
@@ -176,107 +192,139 @@ export default class StagingProduk extends Component {
               }}
             />
           }>
-          {this.state.daftar_pembelian == null ? (
-            <View style={{alignSelf: 'center'}}>
-              <Text>Daftar Kosong</Text>
-            </View>
+          {this.state.loading ? (
+            <ActivityIndicator
+              size="large"
+              color="tomato"
+              style={{margin: 10}}
+            />
           ) : (
-            <Text>
-              {this.state.loadingConfirm ? (
-                <View style={styles.button}>
-                  <ActivityIndicator size="large" color="white" />
+            <View>
+              {this.state.daftar_pembelian == null ? (
+                <View style={{alignItems: 'center', width: '100%'}}>
+                  <Text>Daftar Kosong</Text>
                 </View>
               ) : (
-                <TouchableNativeFeedback onPress={() => this.confirmProduk()}>
-                  <View style={{...styles.button}}>
-                    <Text style={styles.text}>Konfirmasi Pembelian</Text>
-                  </View>
-                </TouchableNativeFeedback>
-              )}
-              <View>
-                {this.state.daftar_pembelian.map((value, index) => (
-                  <View key={index} style={styles.viewProduct}>
-                    <Text>Nama Produk: {value.name}</Text>
-                    {value.jumlah_product <= 1 ? (
-                      <Text>
-                        Stok Produk:{' '}
-                        <Text style={{color: 'tomato'}}>
-                          {this.toPrice(value.jumlah_product)}
-                        </Text>
-                      </Text>
-                    ) : (
-                      <Text>
-                        Stok Produk: {this.toPrice(value.jumlah_product)}
-                      </Text>
-                    )}
-                    <Text>Perbarui Stok</Text>
-                    <TextInput
-                      underlineColorAndroid="orange"
-                      placeholder="Stok Barang"
-                      keyboardType="number-pad"
-                      onChangeText={(input) =>
-                        this.setState({jumlah_product: input})
-                      }
-                    />
-                    {value.harga <= 1 ? (
-                      <Text>
-                        Harga Satuan Produk:{' '}
-                        <Text style={{color: 'tomato'}}>
-                          Rp {this.toPrice(value.harga)},-
-                        </Text>
-                      </Text>
-                    ) : (
-                      <Text>
-                        Harga Satuan Produk: Rp {this.toPrice(value.harga)},-
-                      </Text>
-                    )}
-                    <Text>Perbarui Harga Satuan</Text>
-                    <TextInput
-                      underlineColorAndroid="orange"
-                      placeholder="Harga Barang"
-                      keyboardType="number-pad"
-                      onChangeText={(input) => this.setState({harga: input})}
-                    />
-                    {value.harga_jual <= 1 ? (
-                      <Text>
-                        Stok Produk:{' '}
-                        <Text style={{color: 'tomato'}}>
-                          {this.toPrice(value.harga_jual)}
-                        </Text>
-                      </Text>
-                    ) : (
-                      <Text>
-                        Stok Produk: Rp {this.toPrice(value.harga_jual)},-
-                      </Text>
-                    )}
-                    <Text>Perbarui Harga Jual</Text>
-                    <TextInput
-                      underlineColorAndroid="orange"
-                      placeholder="Harga Jual Barang"
-                      keyboardType="number-pad"
-                      onChangeText={(input) =>
-                        this.setState({harga_jual: input})
-                      }
-                    />
-                    <View
-                      style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-evenly',
-                      }}>
-                      <Button
-                        color="tomato"
-                        title="hapus produk"
-                        onPress={() => this.deleteProduk(value.id)}
-                      />
-                      <Button
-                        title="perbarui produk"
-                        onPress={() => this.updateProduk(value.id)}
-                      />
+                <View>
+                  <TouchableNativeFeedback onPress={() => this.confirmProduk()}>
+                    <View style={{...styles.button}}>
+                      <Text style={styles.text}>Konfirmasi Pembelian</Text>
                     </View>
+                  </TouchableNativeFeedback>
+                  <View>
+                    {this.state.daftar_pembelian.map((value, index) => (
+                      <View key={index} style={styles.viewProduct}>
+                        <Text style={{marginBottom: 10}}>
+                          Nama Produk:{' '}
+                          <Text style={{fontWeight: 'bold'}}>{value.name}</Text>
+                        </Text>
+                        {value.jumlah_product <= 1 ? (
+                          <Text>
+                            Stok Produk:{' '}
+                            <Text style={{color: 'tomato'}}>
+                              {this.toPrice(value.jumlah_product)}
+                            </Text>
+                          </Text>
+                        ) : (
+                          <Text>
+                            Stok Produk:{' '}
+                            <Text style={{fontWeight: 'bold'}}>
+                              {this.toPrice(value.jumlah_product)}
+                            </Text>
+                          </Text>
+                        )}
+                        <View
+                          style={{flexDirection: 'row', alignItems: 'center'}}>
+                          <Text>Perbarui: </Text>
+                          <TextInput
+                            style={{flex: 1}}
+                            underlineColorAndroid="orange"
+                            placeholder="Stok Barang"
+                            keyboardType="number-pad"
+                            onChangeText={(input) =>
+                              this.setState({jumlah_product: input})
+                            }
+                          />
+                        </View>
+                        {value.harga <= 1 ? (
+                          <Text>
+                            Harga Satuan Produk:{' '}
+                            <Text style={{color: 'tomato'}}>
+                              Rp {this.toPrice(value.harga)},-
+                            </Text>
+                          </Text>
+                        ) : (
+                          <Text>
+                            Harga Satuan Produk: Rp{' '}
+                            <Text style={{fontWeight: 'bold'}}>
+                              {this.toPrice(value.harga)}
+                            </Text>
+                            ,-
+                          </Text>
+                        )}
+                        <View
+                          style={{flexDirection: 'row', alignItems: 'center'}}>
+                          <Text>Perbarui: </Text>
+                          <TextInput
+                            style={{flex: 1}}
+                            underlineColorAndroid="orange"
+                            placeholder="Harga Barang"
+                            keyboardType="number-pad"
+                            onChangeText={(input) =>
+                              this.setState({harga: input})
+                            }
+                          />
+                        </View>
+                        {value.harga_jual <= 1 ? (
+                          <Text>
+                            Harga Jual Produk:{' '}
+                            <Text style={{color: 'tomato'}}>
+                              Rp {this.toPrice(value.harga_jual)},-
+                            </Text>
+                          </Text>
+                        ) : (
+                          <Text>
+                            Harga Jual Produk: Rp{' '}
+                            <Text style={{color: 'green', fontWeight: 'bold'}}>
+                              {this.toPrice(value.harga_jual)}
+                            </Text>
+                            ,-
+                          </Text>
+                        )}
+                        <View
+                          style={{flexDirection: 'row', alignItems: 'center'}}>
+                          <Text>Perbarui:</Text>
+                          <TextInput
+                            style={{flex: 1}}
+                            underlineColorAndroid="orange"
+                            placeholder="Harga Jual Barang"
+                            keyboardType="number-pad"
+                            onChangeText={(input) =>
+                              this.setState({harga_jual: input})
+                            }
+                          />
+                        </View>
+                        <View
+                          style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-evenly',
+                          }}>
+                          <Button
+                            color="tomato"
+                            title="hapus produk"
+                            onPress={() => this.deleteProduk(value.id)}
+                          />
+                          <Button
+                            title="perbarui produk"
+                            onPress={() => this.updateProduk(value.id)}
+                          />
+                        </View>
+                      </View>
+                    ))}
                   </View>
-                ))}
-              </View>
-            </Text>
+                </View>
+              )}
+            </View>
           )}
           <Modal
             visible={this.state.modal}
@@ -327,7 +375,7 @@ const styles = StyleSheet.create({
     elevation: 3,
     borderRadius: 10,
     padding: 10,
-    marginBottom: 10,
+    margin: 10,
   },
   viewModal: {
     backgroundColor: 'white',
@@ -337,7 +385,7 @@ const styles = StyleSheet.create({
     width: '90%',
   },
   button: {
-    marginBottom: 10,
+    marginTop: 10,
     padding: 10,
     borderRadius: 10,
     elevation: 3,
