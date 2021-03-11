ed6636b6c2004ec635b9efb54ba970a825a0fba9
RadiantF <radiantfadilah0@gmail.com>
2021-03-12 06:23:29 +0700

    testing qr code

--- src/Demo.js
+++ src/Demo.js
@@ -1,38 +1,62 @@
+// import React, {Component} from 'react';
+// import {Text, View} from 'react-native';
+
+// export default class extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       date: '',
+//       month: '',
+//       months: ['01', '02', '03', '04', '05', '06'],
+//       year: '',
+//     };
+//   }
+
+//   componentDidMount = () => {
+//     console.log('mengambil tanggal hari ini..');
+//     var date = new Date().getDate();
+//     var month = new Date().getMonth() + 1;
+//     var year = new Date().getFullYear();
+//     this.setState({date: date, month: month, year: year});
+//   };
+
+//   getMonth = () => {
+//     console.log('mengambil bulan..');
+//     return this.state.months
+//       .filter((item, index) => this.state.month == index + 1)
+//       .map((v, i) => {
+//         return this.state.date + '-' + v + '-' + this.state.year;
+//       });
+//   };
+
+//   render() {
+//     return (
+//       <View>
+//         <Text>{this.state.date}</Text>
+//         <Text>{this.state.month}</Text>
+//         <Text>{this.state.year}</Text>
+//         <Text>{this.getMonth()}</Text>
+//       </View>
+//     );
+//   }
+// }
+
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-import DatePicker from 'react-native-datepicker';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class Demo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      date: '2016-05-15',
-    };
+  qrCode(code) {
+    console.log(code);
+    alert(code.data);
   }
+
   render() {
-    console.log(this.state.date);
     return (
-      <DatePicker
-        date={this.state.date}
-        mode="date"
-        placeholder="pilih tanggal"
-        format="YYYY-MM-DD"
-        minDate="2016-05-01"
-        maxDate="2021-02-25"
-        confirmBtnText="Konfirmasi"
-        cancelBtnText="Batal"
-        customStyles={{
-          dateIcon: {
-            position: 'absolute',
-            left: 0,
-            top: 4,
-            marginLeft: 0,
-          },
-          dateInput: {
-            marginLeft: 36,
-          },
-        }}
-        onDateChange={(date) => this.setState({date: date})}
+      <QRCodeScanner
+        cameraStyle={{width: '100%'}}
+        showMarker
+        onRead={(code) => this.qrCode(code)}
       />
     );
   }
